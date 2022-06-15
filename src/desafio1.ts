d088d2ec3fd8d607e701a5492b98dd7fc06acf1f
Adriel <adriel.fariasan@gmail.com>
2022-06-14 23:13:49 -0300

    Adiciona solucao do desafio1

---
+++ src/desafio1.ts
@@ -0,0 +1,16 @@
+/*
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+let employee = {};
+employee.code = 10;
+employee.name = "John";
+*/
+
+interface Employee {
+  code?: number;
+  name?: string;
+}
+
+let employee: Employee = {};
+employee.code = 10;
+employee.name = 'John';
